08f3f734e2f88b47d907984240117a17b33620b9
Yogita <Yogita@TYSS-Yogita>
2021-12-10 10:20:04 +0530

    Renamed Classes

---
+++ Test/src/main/java/utility/JsonUtility.java
@@ -0,0 +1,21 @@
+package utility;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class JsonUtility {
+	public static String readDataFromJson(String jsonKey) throws FileNotFoundException, IOException, ParseException {
+		FileReader reader=new FileReader(".\\src\\main\\resources\\AppData.json");
+		JSONParser parse=new JSONParser();
+		Object obj = parse.parse(reader);
+		JSONObject jsonData = (JSONObject)obj;
+		String name = jsonData.get(jsonKey).toString();
+		return name;
+	}
+
+}
---
+++ Test/src/main/java/utility/ListenerIMP.java
@@ -0,0 +1,41 @@
+package utility;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenerIMP implements ITestListener {
+	
+public void onTestStart(ITestResult result) {
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		
+	}
+
+	public void onTestFailedWithTimeout(ITestResult result) {
+		
+	}
+
+	public void onStart(ITestContext context) {
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		
+	}
+
+}
